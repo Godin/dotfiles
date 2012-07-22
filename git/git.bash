39eecebcd2abe90ab6734b33caecfff2f9e7ec7a
Evgeny Mandrikov <mandrikov@gmail.com>
2012-07-23 00:12:11 +0600

    Integrate Git into Bash prompt

---
+++ git/git.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function __dotfiles_workdir() {
+  local g="$(git rev-parse --git-dir 2>/dev/null)"
+  if [ -n "$g" ]; then
+    # current working directory in repository
+    local subdir=$(git rev-parse --show-prefix 2>/dev/null)
+    subdir="${subdir%/}"
+    local workdir="${PWD%/$subdir}"
+    printf "%s" "${workdir/*\/}${subdir:+/$subdir}"
+  else
+    # current working directory, with $HOME abbreviated with a tilde
+    printf "%s" $(echo $PWD | sed -e "s#^$HOME#~#g")
+  fi
+}
+
+GIT_PS1_SHOWUPSTREAM="verbose"
+GIT_PS1_SHOWDIRTYSTATE=true
+PS1='\t \e[1;34m$(__dotfiles_workdir) \e[0m$(__git_ps1 "[%s] ")\e[1;34m\$ \e[0m'
