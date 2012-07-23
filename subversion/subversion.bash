4fa7ade93a60cdbc6da94737216fe707f6abe37f
Evgeny Mandrikov <mandrikov@gmail.com>
2012-07-23 17:05:21 +0600

    Improve script for integration with Subversion
    * Paginate only if there is more than one screen
    * Add colorization for svn log
    * Improve readability of script

--- subversion/subversion.bash
+++ subversion/subversion.bash
@@ -1,37 +1,45 @@
 #!/bin/bash
 
 function svn() {
-    local SVN="`which svn`"
     if [[ ! -t 1 ]]; then
       # not terminal, so can not use colors for pager
       # pass as is
-      ${SVN} "$@"
+      command svn "$@"
       return
     fi
 
-    if [[ $1 == diff ]] ; then
-      # colorize
-      ${SVN} diff "${@:2}" | colordiff | less -R
-    elif [[ $1 == st ]] ; then
-      # colorize
-      ${SVN} st --ignore-externals "${@:2}" | grep -v '^X' | sed -e 's/^\?.*$/\0/' -e 's/^!.*$/\x1b[31m\0\x1b[0m/' -e 's/^A.*$/\x1b[32m\0\x1b[0m/' -e 's/^M.*$/\x1b[33m\0\x1b[0m/' -e 's/^D.*$/\x1b[31m\0\x1b[0m/'
+    if [ $# -lt 1 ]; then
+      # no sub-command
+      command svn
+      return
+    fi
+    local sub_cmd=$1
+    shift
 
-    elif [[ $1 == log ]] ; then
-      ${SVN} log "${@:2}" | less
+    if [[ $sub_cmd == diff ]] ; then
+      # colorize
+      command svn diff "$@" | colordiff | less -RXF
 
-    elif [[ $1 == up ]] ; then
-      local old_revision=`${SVN} info "${@:2}" | awk '/^Revision:/ {print $2}'`
+    elif [[ $sub_cmd == st ]] ; then
+      # colorize
+      command svn st --ignore-externals "$@" | grep -v '^X' | sed -e 's/^\?.*$/\0/' -e 's/^!.*$/\x1b[31m\0\x1b[0m/' -e 's/^A.*$/\x1b[32m\0\x1b[0m/' -e 's/^M.*$/\x1b[33m\0\x1b[0m/' -e 's/^D.*$/\x1b[31m\0\x1b[0m/'
+
+    elif [[ $sub_cmd == log ]] ; then
+      command svn log "$@" | sed -e 's/^--*/\x1b[0;34m\0\x1b[0m/' -e 's/^\([^|]*\) | \(.*\)/\x1b[0;32m\1\x1b[0m | \2/' | less -RXF
+
+    elif [[ $sub_cmd == up ]] ; then
+      local old_revision=`command svn info "$@" | awk '/^Revision:/ {print $2}'`
       local first_update=$((${old_revision} + 1))
-      ${SVN} up "${@:2}"
-      local new_revision=`${SVN} info "${@:2}" | awk '/^Revision:/ {print $2}'`
+      command svn up "$@"
+      local new_revision=`command svn info "$@" | awk '/^Revision:/ {print $2}'`
       if [ ${new_revision} -gt ${old_revision} ]; then
-        svn log -v -rHEAD:${first_update} "${@:2}"
+        svn log -v -rHEAD:${first_update} "$@"
       else
         echo "No changes."
       fi
 
     else
       # pass as is
-      ${SVN} "$@"
+      command svn $sub_cmd "$@"
     fi
 }
