08bb337559042ada2d83d03dfcc0482c0fd23965
Evgeny Mandrikov <mandrikov@gmail.com>
2012-11-20 22:47:29 +0100

    Settings for OS X

---
+++ osx/homebrew.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if test ! $(which brew)
+then
+    echo "You should probably install Homebrew first:"
+    echo "https://github.com/mxcl/homebrew/wiki/installation"
+    exit
+fi
+
+brew install bash
---
+++ osx/set-defaults.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Sets reasonable OS X defaults.
+#
+# Inspired by:
+# https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#
+
+# Finder: allow quitting via Cmd + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
