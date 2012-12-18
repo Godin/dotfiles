3f69897844f9f20084446f9cb1868030d07a9ac6
Evgeny Mandrikov <mandrikov@gmail.com>
2012-12-18 08:36:14 +0100

    Install packages for OS X

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
 
-if test ! $(which brew)
-then
+command -v brew >/dev/null 2>&1
+if [ "$?" -ne "0" ]; then
     echo "You should probably install Homebrew first:"
     echo "https://github.com/mxcl/homebrew/wiki/installation"
     exit
 fi
 
-brew install coreutils
+brew update
+brew upgrade
+brew cleanup
 
-brew install bash
+function install {
+  brew ls $1 >/dev/null 2>&1
+  if [ "$?" -ne "0" ]; then
+    brew install $1
+  else
+    echo "$1 already installed"
+  fi
+}
+
+install coreutils
+
+install bash
+install bash-completion
+
+install midnight-commander
+
+install gawk
+install maven
+
+install git
+install mercurial
+install subversion
+
+install atool
+install wget
+
+install v8
+
+# For slices-of-life
+install imagesnap
